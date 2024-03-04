5f2aae8ab6d6e400fc2a5df4a1b4ecc76011f16c
renato <renatocarrino@gmail.com>
2024-03-04 11:32:40 -0300

    move LoadUserByEmailRepository to its own file

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ const config = {
   collectCoverageFrom: ['**/src/**/*.js'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  preset: '@shelf/jest-mongodb'
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig']
 }
 
 module.exports = config
