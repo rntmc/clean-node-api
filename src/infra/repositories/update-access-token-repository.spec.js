aff4ec901c042988d0a0a78d181d2f47cfbc3080
renato <renatocarrino@gmail.com>
2024-03-04 14:22:10 -0300

    move sut creation to a factory helper method

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -16,23 +16,31 @@ class UpdateAccessTokenRepository {
   }
 }
 
+const makeSut = () => {
+  const userModel = db.collection('users')
+  const sut = new UpdateAccessTokenRepository(userModel)
+  return {
+    userModel,
+    sut
+  }
+}
+
 describe('UpdateAccessToken Repository', () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-      db = await MongoHelper.getDb()
-    })
-  
-    beforeEach(async () => {
-      await db.collection('users').deleteMany()
-    })
-  
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    db = await MongoHelper.getDb()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
 
   test('Should update the user with the given accessToken', async () => {
-    const userModel = db.collection('users')
-    const sut = new UpdateAccessTokenRepository(userModel)
+    const { sut, userModel } = makeSut()
     const fakeUser = await userModel.insertOne({
       email: 'valid_email@mail.com',
       name: 'any_name',
