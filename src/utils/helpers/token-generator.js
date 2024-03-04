33eed2652f57edf9d90fd42133a573a37168d40f
renato <renatocarrino@gmail.com>
2024-03-04 10:33:23 -0300

    move TokenGenerator to its own file

---
+++ src/utils/helpers/token-generator.js
@@ -0,0 +1,15 @@
+module.exports = class TokenGenerator {
+  constructor(secret) {
+    this.secret = secret
+  }
+
+  async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+    return jwt.sign(id, this.secret)
+  }
+}
