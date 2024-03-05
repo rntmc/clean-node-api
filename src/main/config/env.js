a982a178f9e58f997d5b6bdbf56ddf0abb39d10d
renato <renatocarrino@gmail.com>
2024-03-05 12:49:23 -0300

    fix mongodb port and app port

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
-  tokenSecret: process.env.TOKEN_SECRET || 'secret'
+  tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  port: process.env.PORT || 5858
 }
